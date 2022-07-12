90f6f0f3ba550fe817b459c1e3500f6475ba0d41
Fashoyin Dunbarin <102663163+dunbarin@users.noreply.github.com>
2022-07-12 22:12:49 +0000

    assignment complete

--- src/assignment.js
+++ src/assignment.js
@@ -14,35 +14,15 @@ const assignment = {};
  * @returns number the sum of the numbers
  */
 
-function sumOfNumbers(array){
+function sumOfNumbers(arrayOfNumbers){
   let sum=0;
-  for(let a of array)
-  sum +=a;
+  for(let i=0; i<arrayOfNumbers.length; i++){
+  sum +=arrayOfNumbers[i];
+}
   return sum;
 }
 
-console.log(sumOfNumbers([
-   68,
-        -68,
-        27,
-        94,
-        72,
-        -25,
-        -51,
-        32,
-        10,
-        64,
-        -94,
-        4,
-        34,
-        -86,
-        90,
-        81,
-        20,
-        -56,
-        -91,
-        -50
-]))
+assignment.sumOfNumbers=sumOfNumbers
 
 
 /**
@@ -55,38 +35,17 @@ console.log(sumOfNumbers([
  * @returns number the count of even numbers
  */
 function countEvenNumbers(arrayOfNumbers) {
-  // let numbers=[0]
-  for (let i=0;i<=arrayOfNumbers.length; i++)
-    if (arrayOfNumbers%2===0){
-    i++
-    } 
-  
-    return arrayOfNumbers.length;
+  let evenNumbers=0;
+  for (let i=0;i<=arrayOfNumbers.length; i++){
+    if (arrayOfNumbers[i]%2==0){
+    evenNumbers++
+    }
+  }
+    return evenNumbers;
 }
 
-console.log (countEvenNumbers([
-        17,
-        0,
-        67,
-        41,
-        49,
-        21,
-        1,
-        86,
-        76,
-        2,
-        54,
-        14,
-        93,
-        84,
-        14,
-        77,
-        35,
-        7,
-        29,
-        64
-    ]))
-// assignment.countEvenNumbers = countEvenNumbers;
+assignment.countEvenNumbers = countEvenNumbers
+
 
 /**
  * Challenge - 3
