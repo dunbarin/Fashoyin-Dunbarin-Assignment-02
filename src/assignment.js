4f2b7db726bba5b21855d2909caf1d5b156f48ad
Fashoyin Dunbarin <102663163+dunbarin@users.noreply.github.com>
2022-07-12 00:36:30 +0000

    retrying number sum

--- src/assignment.js
+++ src/assignment.js
@@ -13,10 +13,37 @@ const assignment = {};
  * @param {Array} arrayOfNumbers the array of numbers to sum
  * @returns number the sum of the numbers
  */
-function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+
+function sumOfNumbers(array){
+  let sum=0;
+  for(let a of array)
+  sum +=a;
+  return sum;
 }
-// assignment.sumOfNumbers = sumOfNumbers;
+
+console.log(sumOfNumbers([
+   68,
+        -68,
+        27,
+        94,
+        72,
+        -25,
+        -51,
+        32,
+        10,
+        64,
+        -94,
+        4,
+        34,
+        -86,
+        90,
+        81,
+        20,
+        -56,
+        -91,
+        -50
+]))
+
 
 /**
  * Challenge - 2
@@ -28,8 +55,37 @@ function sumOfNumbers(arrayOfNumbers) {
  * @returns number the count of even numbers
  */
 function countEvenNumbers(arrayOfNumbers) {
+  // let numbers=[0]
+  for (let i=0;i<=arrayOfNumbers.length; i++)
+    if (arrayOfNumbers%2===0){
+    i++
+    } 
+  
     return arrayOfNumbers.length;
 }
+
+console.log (countEvenNumbers([
+        17,
+        0,
+        67,
+        41,
+        49,
+        21,
+        1,
+        86,
+        76,
+        2,
+        54,
+        14,
+        93,
+        84,
+        14,
+        77,
+        35,
+        7,
+        29,
+        64
+    ]))
 // assignment.countEvenNumbers = countEvenNumbers;
 
 /**
